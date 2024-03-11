4aeb634ac8d72737f0a65dd42795305322c03a0f
techcursed <31003460+TechCursed@users.noreply.github.com>
2024-03-11 15:58:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,54 @@
+let inputData = "";
+let summarised = "";
 
+//FUNCTION EXECUTION AFTER TEXT IS SUBMITTED 
 
-//FUNCTION FOR SUMMARISING THE INPUT TEXT
+document.getElementById("submitButton").addEventListener("click", async () => {
+    const textInput = document.getElementById("textInput").value;
+    inputData = textInput;
+
+    // IF INPUT TEXT IS NOT EMPTY
+    if (textInput.trim() !== "") {
+        try {
+
+            // LOADER UNTIL THE DATA IS BEING FETCHED
+            const loadingMessage = document.createElement("p");
+            loadingMessage.textContent = "Loading...";
+          
+            document.body.appendChild(loadingMessage);
+            const response = await query({ inputs: textInput });
+            console.log(JSON.stringify(response));
+
+            summarised = JSON.stringify(response);
+           let trimmedText = JSON.parse(summarised)[0].summary_text.trim();
+          
+            const h4 = document.createElement("h4");
+            h4.textContent = trimmedText;
+
+            //REMOVING THE LOADER TEXT AFTER GETTING THE RESPONSE
+            document.body.removeChild(loadingMessage);
+
+            // DISPLAYING THE RESPONSE ON THE HTML
+            document.body.appendChild(h4);
+        } catch (error) {
+            console.error("Error:", error.message);
+        }
+    } else {
+        console.log("Please enter some text");
+    }
+});
+
+//FUNCTION FOR THE SUMMARISATION
 
 async function query(data) {
-  const response = await fetch(
-    "https://api-inference.huggingface.co/models/facebook/bart-large-cnn",
-    {
-      headers: { Authorization: "Bearer " },
-      method: "POST",
-      body: JSON.stringify(data),
-    }
-  );
-  const result = await response.json();
-  return result;
+    const response = await fetch(
+        "https://api-inference.huggingface.co/models/facebook/bart-large-cnn",
+        {
+            headers: { Authorization: "Bearer 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" },
+            method: "POST",
+            body: JSON.stringify(data),
+        }
+    );
+    const result = await response.json();
+    return result;
 }
-
-query({"inputs": "The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building, and the tallest structure in Paris. Its base is square, measuring 125 metres (410 ft) on each side. During its construction, the Eiffel Tower surpassed the Washington Monument to become the tallest man-made structure in the world, a title it held for 41 years until the Chrysler Building in New York City was finished in 1930. It was the first structure to reach a height of 300 metres. Due to the addition of a broadcasting aerial at the top of the tower in 1957, it is now taller than the Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France after the Millau Viaduct."}).then((response) => {
-  console.log(JSON.stringify(response));
-});
